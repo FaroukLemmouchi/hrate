bf379355643f20469f3a7dbe71e6fc2951782713
farouk <farouklem@yahoo.fr>
2024-06-07 22:36:41 +0200

    mock works but time still seconds.

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,84 +1,97 @@
-async function connect(props) {
-    const device = await navigator.bluetooth.requestDevice({
-      filters: [{ services: ['heart_rate'] }],
-      acceptAllDevices: false,
-    })
-    const server = await device.gatt.connect()
-    const service = await server.getPrimaryService('heart_rate')
-    const char = await service.getCharacteristic('heart_rate_measurement')
-    char.oncharacteristicvaluechanged = props.onChange
-    char.startNotifications()
-    alert('Starting heart rate measurement in few seconds...\nREFRESH the page to STOP.')
-    return char
-  }
+// async function connect(props) {
+//     const device = await navigator.bluetooth.requestDevice({
+//       filters: [{ services: ['heart_rate'] }],
+//       acceptAllDevices: false,
+//     })
+//     const server = await device.gatt.connect()
+//     const service = await server.getPrimaryService('heart_rate')
+//     const char = await service.getCharacteristic('heart_rate_measurement')
+//     char.oncharacteristicvaluechanged = props.onChange
+//     char.startNotifications()
+//     // alert('Starting heart rate measurement in few seconds...\nREFRESH the page to STOP.')
+//     return char
+//   }
 
-  document.getElementById('connectButton').addEventListener('click', function() {
-    connect({ onChange: printHeartRate })});
+//   document.getElementById('connectButton').addEventListener('click', function() {
+//     connect({ onChange: printHeartRate })});
 
-    let time = 0;
-    function printHeartRate(event) {
-        const heartRate = event.target.value.getInt8(1)
-        console.clear()
-            addData(liveChart, time, heartRate);
-            time += 1;
-  }
+    const time = Date.now();
+//     function printHeartRate(event) {
+//         const heartRate = event.target.value.getInt8(1)
+//             addData(liveChart, time, heartRate);
+//             time += 1;
+//   }
   
 const ctx = document.getElementById('liveChart').getContext('2d');
 const data = {
     labels: [],
     datasets: [{
-        //label: 'Live Data',
         data: [],
-        borderColor: 'rgba(75, 192, 192, 1)',
-        borderWidth: 1,
-        //fill: true
     }]
 };
 
 const config = {
-    type: 'line',
-    data: data,
-    options: {
-        scales: {
-            x: {
-                type: 'linear',
-                position: 'bottom',
-            },
-        },
-        plugins: {
-            legend: {
-                display: false,
-            }
-        }
-    }
+  type: 'line',
+  data: data,
+  options: {
+      scales: {
+          x: {
+              type: 'linear',  // Use linear type for x-axis
+              positiontop: 'bottom',
+              ticks: {
+                  autoSkip: true,
+                  maxTicksLimit: 10
+              }
+          },
+          y: {
+              min: 0,
+              max: 100
+          }
+      }
+  }
 };
 
-const liveChart = new Chart(ctx, config);
 
-function addData(chart, label, data) {
+const chart = new Chart(ctx, config);
+
+// Update the chart every second (1000 milliseconds)
+setInterval(updateChart, 1000);
+
+function addData(chart, data) {
     document.getElementById("hr-value").innerText = data;
-    chart.data.labels.push(label);
+    const time = Date.now()
+
+    chart.data.labels.push(time);
     chart.data.datasets.forEach((dataset) => {
         dataset.data.push(data);
     });
-    chart.update();
+    chart.options.scales.x.min = time - 10000;
+    chart.options.scales.x.max = time;
+    console.log(time)
 }
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('/service-worker.js')
-      .then(function(registration) {
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      }, function(err) {
-        console.log('ServiceWorker registration failed: ', err);
-      });
-    }
+
+// Function to update the chart with new data and adjust the x-axis range
+function updateChart() {
+  const data = Math.floor(Math.random() * 100) // Random data for example
+  addData(chart, data);
+  chart.update();
+}
+
+//     if ('serviceWorker' in navigator) {
+//       navigator.serviceWorker.register('/service-worker.js')
+//       .then(function(registration) {
+//         console.log('ServiceWorker registration successful with scope: ', registration.scope);
+//       }, function(err) {
+//         console.log('ServiceWorker registration failed: ', err);
+//       });
+//     }
 
 
-    // script.js
-document.addEventListener('DOMContentLoaded', (event) => {
-    const toggleButton = document.getElementById('night-mode-toggle');
-    const body = document.body;
+// document.addEventListener('DOMContentLoaded', (event) => {
+//     const toggleButton = document.getElementById('night-mode-toggle');
+//     const body = document.body;
 
-    toggleButton.addEventListener('click', () => {
-        body.classList.toggle('night-mode');
-    });
-});
+//     toggleButton.addEventListener('click', () => {
+//         body.classList.toggle('night-mode');
+//     });
+// });
--- run.py
+++ run.py
@@ -4,5 +4,5 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run() #debug=False, ssl_context=('cert.pem', 'key.pem')) #, host='0.0.0.0')
+    app.run(debug=True)#, ssl_context=('cert.pem', 'key.pem')) #, host='0.0.0.0')
     
