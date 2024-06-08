c07b35e096c0bab6519d751fb53d7b1badb208ee
farouk <farouklem@yahoo.fr>
2024-06-08 02:22:52 +0200

    two graph works same yaxis, seconds xaxis

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -15,7 +15,7 @@
 //   document.getElementById('connectButton').addEventListener('click', function() {
 //     connect({ onChange: printHeartRate })});
 
-const time = new Date(Date.now())
+let time = 0
 //     function printHeartRate(event) {
 //         const heartRate = event.target.value.getInt8(1)
 //             addData(liveChart, time, heartRate);
@@ -25,41 +25,97 @@ const time = new Date(Date.now())
 const ctx = document.getElementById('liveChart').getContext('2d');
 const data = {
     labels: [],
-    datasets: [{
-        data: [],
-    }]
+    datasets: [
+        {
+            label: 'Heart Rate',
+            data: [], // Array to hold dataset values
+            borderColor: 'rgb(255, 0, 0)',
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+            borderWidth: 1,
+            fill: false, // No fill under the line
+            yAxisID: 'y-axis-1'
+
+        },
+        {
+            label: '2nd graph',
+            data: [], // Array to hold dataset values
+            borderColor: 'rgb(75, 192, 0)',
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+            borderWidth: 1,
+            fill: false, // No fill under the line
+            yAxisID: 'y-axis-2'
+
+        },]
 };
 
 const config = {
     type: 'line',
     data: data,
     options: {
+        x: {
+            title: {
+                display: true,
+                text: 'Time'
+            },
+        },
+        'y-axis-1': {
+            type: 'linear',
+            display: true,
+            position: 'left',
+            options: {
+
+                min: 30,
+                max: 200,
+            },
+            title: {
+                display: true,
+                text: 'Value for Dataset 1'
+            }
+        },
+        'y-axis-2': {
+            type: 'linear',
+            display: true,
+            position: 'right',
+            options: {
+
+                min: 30,
+                max: 200,
+            },
+
+            title: {
+                display: true,
+                text: 'Value for Dataset 2'
+            },
+            grid: {
+                drawOnChartArea: false  // only want the grid lines for one axis to show up
+            }
+        }
     }
 };
 
 const chart = new Chart(ctx, config);
 
 // Update the chart every second (1000 milliseconds)
-setInterval(updateChart, 1000);
+setInterval(updateChart, 500);
 
-function addData(chart, data) {
+function addData(chart, datalist) {
+    const data = datalist[0]
+    const data2 = datalist[1]
     document.getElementById("hr-value").innerText = data;
-    const time = new Date(Date.now())
+    time += 1
 
-    chart.data.labels.push(time.toISOString().substring(11, 19));
-    console.log(chart.data.labels)
-    chart.data.datasets.forEach((dataset) => {
-        dataset.data.push(data);
-    });
-    //chart.options.scales.x.min = time.setMinutes(time.getMinutes() - 5);;
-    chart.options.scales.x.max = time;
-    console.log(time)
+    chart.data.labels.push(time);
+    chart.data.datasets[1].data.push(data2)
+    chart.data.datasets[0].data.push(data)
+
+    chart.options.scales.x.min = Math.max(0, time - 100000);
 }
 
 // Function to update the chart with new data and adjust the x-axis range
 function updateChart() {
-    const data = Math.floor(Math.random() * 100) // Random data for example
-    addData(chart, data);
+    const data = Math.floor(Math.random() * 100 + 100) // Random data for example
+    const data2 = Math.floor(Math.random() * 100 + 80) // Random data for example
+    addData(chart, [data, data2]);
     chart.update();
 }
 
